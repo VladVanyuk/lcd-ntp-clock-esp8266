5f12df36305f30b1863308a0e7c103d75951361a
VladVanyuk <vladislavvanyuk@gmail.com>
2026-01-24 12:41:28 +0200

    added pio project; added lcd lib

---
+++ lcd-ntp/lib/LiquidCrystal/src/LiquidCrystal_Base.h
@@ -0,0 +1,11 @@
+
+#pragma once
+#include <Arduino.h>
+
+#define LIQUIDCRYSTAL_BASE_VERSION	"1.2.1"
+
+#include "coreLCD/LCD_defs.h"
+#include "coreLCD/LiquidCrystal_Base.h"
+#include "coreLCD/LiquidCrystal.h"
+#include "coreLCD/LiquidCrystal_I2C.h"
+
